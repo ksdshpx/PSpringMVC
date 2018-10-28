d1149a05e634ded43aaf5b12c050b29a16e1fe12
ksdshpx <southeast_px@163.com>
2018-10-28 16:36:33 +0800

    编写请求处理器！

---
+++ src/cn/ksdshpx/springmvc/helloworld/SpringMVCHandler.java
@@ -0,0 +1,13 @@
+package cn.ksdshpx.springmvc.helloworld;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author peng.x
+ * @date 2018年10月28日 下午4:35:54
+  * 请求处理器
+ */
+@Controller
+public class SpringMVCHandler {
+
+}
